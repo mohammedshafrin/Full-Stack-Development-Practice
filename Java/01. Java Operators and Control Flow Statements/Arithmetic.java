49df4479b3c02e1e070ac1798003600906c8bc70
mohammedshafrin <mohammedshafrin.it@gmail.com>
2026-06-18 12:09:08 +0530

    update

--- Java/01. Java Operators and Control Flow Statements/Arithmetic.java
+++ Java/01. Java Operators and Control Flow Statements/Arithmetic.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Arithmetic {
 
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter a Value");
-        int number = input.nestInt();
+        System.out.println("Enter the first number :");
+        int number1 = input.nextInt();
 
-        System.out.println("You entered :" + number);
+        System.out.println("Enter second number : ");
+        int number2 = input.nextInt();
+
+        int addition = number1 + number2;
+        int subtraction = number1 - number2 ; 
+        int multiplication = number1  * number2; 
+        int division = number1 / number2;
+
+        System.out.println("addition : " + addition);
+        System.out.println("subtraction : " + subtraction);
+        System.out.println("multiplication : " + multiplication);
+        System.out.println("division : " + division);
 
         input.close();
 
